14fcad34aca98fc2e133ef489e33ba2f329a1ce4
michael.lehman <mike.lehman11@gmail.com>
2016-09-22 10:20:01 -0400

    complete R code for hw2

--- hw2/hw2.r
+++ hw2/hw2.r
@@ -11,7 +11,7 @@ sms_raw <- read.csv("sms_spam.csv", stringsAsFactors = FALSE)
 str(sms_raw)
 
 # convert type feature to a factor
-sms_raw$tpye <- factor(sms_raw$type)
+sms_raw$type <- factor(sms_raw$type)
 str(sms_raw$type)
 table(sms_raw$type)
 
@@ -105,5 +105,134 @@ sms_test_pred <- predict(sms_classifier, sms_test)
 CrossTable(sms_test_pred, sms_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
 
 # Improving Performance
-# use custom function to replace punctuation with blank space instead of completely removing punctuation
+# Alternate Solutions
+
+# 1. use custom function to replace punctuation
+
 replacePunctuation <- function(x) { gsub("[[:punct:]]+", " ", x) }
+
+sms_corpus_clean <- tm_map(sms_corpus_clean, replacePunctuation)
+
+sms_corpus_clean <- tm_map(sms_corpus_clean, stemDocument)
+sms_corpus_clean <- tm_map(sms_corpus_clean, stripWhitespace) 
+
+lapply(sms_corpus[1:3], as.character)
+lapply(sms_corpus_clean[1:3], as.character)
+
+sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
+
+sms_dtm_train <- sms_dtm[1:4169, ]
+sms_dtm_test  <- sms_dtm[4170:5559, ]
+
+sms_train_labels <- sms_raw[1:4169, ]$type
+sms_test_labels  <- sms_raw[4170:5559, ]$type
+
+sms_dtm_freq_train <- removeSparseTerms(sms_dtm_train, 0.999)
+
+findFreqTerms(sms_dtm_train, 5)
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+
+sms_dtm_freq_train <- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+
+sms_train <- apply(sms_dtm_freq_train, MARGIN = 2, convert_counts)
+sms_test  <- apply(sms_dtm_freq_test, MARGIN = 2, convert_counts)
+sms_classifier <- naiveBayes(sms_train, sms_train_labels)
+sms_test_pred <- predict(sms_classifier, sms_test)
+CrossTable(sms_test_pred, sms_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
+
+# 2. create DTM directly from the sms_corpus
+
+sms_dtm <- DocumentTermMatrix(sms_corpus, control = list(
+  tolower = TRUE,
+  removeNumbers = TRUE,
+  stopwords = TRUE,
+  removePunctuation = TRUE,
+  stemming = TRUE
+))
+
+sms_dtm_train <- sms_dtm[1:4169, ]
+sms_dtm_test  <- sms_dtm[4170:5559, ]
+
+sms_train_labels <- sms_raw[1:4169, ]$type
+sms_test_labels  <- sms_raw[4170:5559, ]$type
+
+sms_dtm_freq_train <- removeSparseTerms(sms_dtm_train, 0.999)
+
+findFreqTerms(sms_dtm_train, 5)
+
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+sms_dtm_freq_train <- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+
+sms_train <- apply(sms_dtm_freq_train, MARGIN = 2, convert_counts)
+sms_test  <- apply(sms_dtm_freq_test, MARGIN = 2, convert_counts)
+
+sms_classifier <- naiveBayes(sms_train, sms_train_labels)
+sms_test_pred <- predict(sms_classifier, sms_test)
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
+# 3. create DTM directly from sms_corpus and user custom stopwords function 
+
+sms_dtm <- DocumentTermMatrix(sms_corpus, control = list(
+  tolower = TRUE,
+  removeNumbers = TRUE,
+  stopwords = function(x) { removeWords(x, stopwords()) },
+  removePunctuation = TRUE,
+  stemming = TRUE
+))
+
+sms_dtm_train <- sms_dtm[1:4169, ]
+sms_dtm_test  <- sms_dtm[4170:5559, ]
+
+sms_train_labels <- sms_raw[1:4169, ]$type
+sms_test_labels  <- sms_raw[4170:5559, ]$type
+
+sms_dtm_freq_train <- removeSparseTerms(sms_dtm_train, 0.999)
+
+findFreqTerms(sms_dtm_train, 5)
+
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+sms_dtm_freq_train <- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+
+sms_train <- apply(sms_dtm_freq_train, MARGIN = 2, convert_counts)
+sms_test  <- apply(sms_dtm_freq_test, MARGIN = 2, convert_counts)
+
+sms_classifier <- naiveBayes(sms_train, sms_train_labels)
+sms_test_pred <- predict(sms_classifier, sms_test)
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
+# 4. laplace estimators
+
+# laplace = 1
+sms_classifier <- naiveBayes(sms_train, sms_train_labels, laplace = 1)
+sms_test_pred <- predict(sms_classifier, sms_test)
+
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
+# laplace = 2
+sms_classifier <- naiveBayes(sms_train, sms_train_labels, laplace = 2)
+sms_test_pred <- predict(sms_classifier, sms_test)
+
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
