83a9c4f890b5470e21c1ccf73d6b887036ae92f3
michael.lehman <mike.lehman11@gmail.com>
2016-10-06 16:29:00 -0400

    cont. hw3

--- hw3/hw3.R
+++ hw3/hw3.R
@@ -1,48 +1,71 @@
-# load data and examine characteristics
+# Data Collection and Preparation
 credit <- read.csv("credit.csv")
 str(credit)
 
-# examine checking and savings balance vectors
 table(credit$checking_balance)
 table(credit$savings_balance)
 
-# examine two features related to the loan
 summary(credit$months_loan_duration)
 summary(credit$amount)
 
-# examine the class feature we are going to predict
 table(credit$default)
-
-# since the data is sorted by x, set a random seed to reorder the data
-# to build train and test data sets
 set.seed(123)
 
-# initialize a train_sample data set with 900 values raning from 1-1000
 train_sample <- sample(1000, 900)
 str(train_sample)
 
-# create train and test data sets using our sample values to get rows from the
-# credit data frame for each set
 credit_train <- credit[train_sample, ]
 credit_test  <- credit[-train_sample, ]
 
-# check the ratio of class variable: default to ensure they are consistent between train and test
 prop.table(table(credit_train$default))
 prop.table(table(credit_test$default))
 
-# load C50 library for modeling decision tree and create a basic decision tree model
-# pass the entire credit_train data frame to the C5.0 function less the class variable
-# as well as the class variable for which we are predicting
+# Train Model
 library(C50)
 credit_model <- C5.0(credit_train[-17], credit_train$default)
 
-# display details about the tree and summary information
 credit_model
 summary(credit_model)
 
-# create vector for predictions using our credit_model and test data set
 credit_pred <- predict(credit_model, credit_test)
 
-# build corss tabulation to compare model results to actuals
+# Evaluate Model Performance
 library(gmodels)
 CrossTable(credit_test$default, credit_pred, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))
+
+# Improving Performance
+credit_boost10 <- C5.0(credit_train[-17], credit_train$default, trials = 10)
+credit_boost10
+summary(credit_boost10)
+credit_boost_pred10 <- predict(credit_boost10, credit_test)
+CrossTable(credit_test$default, credit_boost_pred10, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))
+
+matrix_dimensions <- list(c("no", "yes"), c("no", "yes"))
+names(matrix_dimensions) <- c("predicted", "actual")
+matrix_dimensions
+
+error_cost <- matrix(c(0, 1, 4, 0), nrow = 2, dimnames = matrix_dimensions)
+error_cost
+
+credit_cost <- C5.0(credit_train[-17], credit_train$default, costs = error_cost)
+credit_cost_pred <- predict(credit_cost, credit_test)
+CrossTable(credit_test$default, credit_cost_pred, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))
+
+# ctree ()
+# Statistics-based approach that uses non-parametric tests as splitting criteria, 
+# corrected for multiple testing to avoid overfitting. 
+# This approach results in unbiased predictor selection and does not require pruning.
+library(party)
+
+ctree_obj <- ctree(default ~ ., data = credit)
+ctree_obj
+plot(ctree_obj)
+
+set.seed(123)
+train_sample <- sample(1000, 900)
+credit_train <- credit[train_sample, ]
+credit_test  <- credit[-train_sample, ]
+ctree_model <- ctree(default ~ ., data = credit_train)
+plot(ctree_model)
+ctree_predict <- predict(ctree_model, credit_test)
+CrossTable(credit_test$default, ctree_predict, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))
