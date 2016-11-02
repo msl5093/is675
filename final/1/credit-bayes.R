1bd8f8223f89d5ae56a6011f66475f7f07fc73e8
michael.lehman <mike.lehman11@gmail.com>
2016-11-02 16:09:07 -0400

    workin' on final

--- final/1/credit-bayes.R
+++ final/1/credit-bayes.R
@@ -9,22 +9,61 @@ library(e1071)
 ##################################################
 credit <- read.csv("credit.csv")
 
+# convert a few features to factors for easier bayesian classification. these have few possible values
+  # so factor makes sense
+credit$percent_of_income <- as.factor(credit$percent_of_income)
+credit$years_at_residence <- as.factor(credit$years_at_residence)
+credit$existing_loans_count <- as.factor(credit$existing_loans_count)
+credit$dependents <- as.factor(credit$dependents)
+
+# summary stats for remaining numeric features - leave as-is for first run
+summary(credit[sapply(credit, is.numeric)])
+
 set.seed(123)
 train_sample <- sample(1000, 900)
 credit_train <- credit[train_sample,]
 credit_test <- credit[-train_sample,]
 
+prop.table(table(credit_train$default))
+prop.table(table(credit_test$default))
+
+credit_train_labels <- credit_train[,"default"]
+credit_test_labels <- credit_test[,"default"]
+
+
+##################################################
+# train model
+##################################################
+classifier <- naiveBayes(credit_train, credit_train_labels)
+class_test_pred <- predict(classifier, credit_test)
+
+
+##################################################
+# evaluate model performance
+##################################################
+CrossTable(class_test_pred, credit_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
+
+
+##################################################
+# imporving model performance
+##################################################
+# smaller train data set to see if first round performance was overfitting
+set.seed(123)
+train_sample <- sample(1000, 500)
+credit_train <- credit[train_sample,]
+credit_test <- credit[-train_sample,]
+
+prop.table(table(credit_train$default))
+prop.table(table(credit_test$default))
+
 credit_train_labels <- credit_train[,"default"]
 credit_test_labels <- credit_test[,"default"]
 
 classifier <- naiveBayes(credit_train, credit_train_labels)
-
 class_test_pred <- predict(classifier, credit_test)
-
 CrossTable(class_test_pred, credit_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
 
-
+# laplace estimators:
 cred_classifier <- naiveBayes(credit_train, credit_train_labels, laplace = 4)
 cred_test_pred <- predict(cred_classifier, credit_test)
-
 CrossTable(cred_test_pred, credit_test_labels, prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
--- final/1/credit-kNN.R
+++ final/1/credit-kNN.R
@@ -10,11 +10,16 @@ library(car)
 # data collection and preparation
 ##################################################
 credit <- read.csv("credit.csv")
+str(credit)
 
+scatterplot(credit$amount ~ credit$months_loan_duration | credit$default) # defaults appear randomly scattered along both amount and months of the loan
+scatterplot(credit$amount ~ credit$age | credit$default) # also random scatter
+
+# how do loan amounts change over the length of the loan
+boxplot(amount ~ months_loan_duration, data = credit, main="Credit Data", xlab = "months_loan_duration", ylab = "amount")
 
 # check for distribution of numeric variables
 summary(credit[sapply(credit, is.numeric)]) # months_loan_duration, amount, age
-
 
 # normalize numeric variables with large range
 normalize <- function(x){
@@ -36,6 +41,7 @@ credit$other_credit <- as.numeric(credit$other_credit)
 credit$housing <- as.numeric(credit$housing)
 credit$job <- as.numeric(credit$job)
 credit$phone <- as.numeric(credit$phone)
+str(credit)
 
 
 # create train and test data sets using random sample
@@ -44,6 +50,8 @@ train_sample <- sample(1000, 900)
 credit_train <- credit[train_sample,]
 credit_test <- credit[-train_sample,]
 
+prop.table(table(credit_train$default))
+prop.table(table(credit_test$default))
 
 # create train and test class objects
 credit_train_labels <- credit_train[,"default"]
@@ -59,12 +67,12 @@ credit_test <- credit_test[-17]
 ##################################################
 # k = sq rt (900 = total train records)
 credit_test_pred <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 30)
-CrossTable(x = credit_test_labels, y = credit_test_pred, prop.chisq = FALSE)
 
 
 ##################################################
 # evaluate model performance
 ##################################################
+CrossTable(x = credit_test_labels, y = credit_test_pred, prop.chisq = FALSE)
 
 
 ##################################################
@@ -72,10 +80,46 @@ CrossTable(x = credit_test_labels, y = credit_test_pred, prop.chisq = FALSE)
 ##################################################
 
 # different number of neighbors
-credit_test_pred2 <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 60)
+credit_test_pred2 <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 40)
 CrossTable(x = credit_test_labels, y = credit_test_pred2, prop.chisq = FALSE)
 
-credit_test_pred3 <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 40)
+credit_test_pred3 <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 20)
 CrossTable(x = credit_test_labels, y = credit_test_pred3, prop.chisq = FALSE)
 
+credit_test_pred4 <- knn(train = credit_train, test = credit_test, cl = credit_train_labels, k = 10)
+CrossTable(x = credit_test_labels, y = credit_test_pred4, prop.chisq = FALSE)
+
+
+
 # z-score normalization
+credit_z <- read.csv("credit.csv")
+credit_z["months_loan_duration"] <- as.data.frame(scale(credit_z["months_loan_duration"]))
+credit_z["amount"] <- as.data.frame(scale(credit_z["amount"]))
+credit_z["age"] <- as.data.frame(scale(credit_z["age"]))
+
+summary(credit_z[sapply(credit_z, is.numeric)]) # months_loan_duration, amount, age
+
+credit_z$checking_balance <- as.numeric(credit_z$checking_balance)
+credit_z$credit_history <- as.numeric(credit_z$credit_history)
+credit_z$purpose <- as.numeric(credit_z$purpose)
+credit_z$savings_balance <- as.numeric(credit_z$savings_balance)
+credit_z$employment_duration <- as.numeric(credit_z$employment_duration)
+credit_z$other_credit <- as.numeric(credit_z$other_credit)
+credit_z$housing <- as.numeric(credit_z$housing)
+credit_z$job <- as.numeric(credit_z$job)
+credit_z$phone <- as.numeric(credit_z$phone)
+
+credit_z_train <- credit_z[train_sample,]
+credit_z_test <- credit_z[-train_sample,]
+
+credit_z_train_labels <- credit_z_train[,"default"]
+credit_z_test_labels <- credit_z_test[,"default"]
+
+credit_z_train <- credit_z_train[-17]
+credit_z_test <- credit_z_test[-17]
+
+credit_z_test_pred <- knn(train = credit_z_train, test = credit_z_test, cl = credit_z_train_labels, k = 30)
+CrossTable(x = credit_z_test_labels, y = credit_z_test_pred, prop.chisq = FALSE)
+
+credit_z_test_pred <- knn(train = credit_z_train, test = credit_z_test, cl = credit_z_train_labels, k = 10)
+CrossTable(x = credit_z_test_labels, y = credit_z_test_pred, prop.chisq = FALSE)
